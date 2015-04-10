8677314615ecf0d18d354f843f448e4fc2b9e9ca
unknown <jonathan.mellon@nuffield.ox.ac.uk>
2015-04-10 11:54:46 -0400

    Making this into an rstudio project

---
+++ R/adjacencyToEdgeList.R
@@ -0,0 +1,19 @@
+adjacencyToEdgeList <- function(adjacency) {
+	edge.list <- t(combn(c(colnames(adjacency), rownames(adjacency)), 2))
+	edge.list <- edge.list[!duplicated(edge.list), ]
+	colnames(edge.list) <- c("source", "target")
+	edge.list <- data.frame(edge.list, stringsAsFactors = FALSE)
+	edge.list$value <- 0
+	
+	for(ii in 1:nrow(edge.list)) {
+		edge.list[ii, "value"] <- adjacency[edge.list[ii, "source"], edge.list[ii, "target"]]
+	}
+	edge.list <- edge.list[edge.list$value!=0, ]
+	return(edge.list)
+}
+
+
+
+
+
+
