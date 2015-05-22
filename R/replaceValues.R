a94891623d658bce162b0357d9273d95ac7a2e59
unknown <jonathan.mellon@nuffield.ox.ac.uk>
2015-05-22 13:36:54 -0400

    Added clustering spell check replacement algorithm for use in automated machine learning.

---
+++ R/replaceValues.R
@@ -0,0 +1,17 @@
+replaceValues <- function(text, subs) {
+  text.list <- strsplit(text, split = " ")
+  
+  replace <- function(part) {
+    tmp.subs <- subs[subs[, 1] %in% part, , drop = FALSE]
+    tmp.subs <- unique(tmp.subs)  
+    if(nrow(tmp.subs)!= 0) {
+      for(i in 1:nrow(tmp.subs)) {
+        part[part==tmp.subs[i, 1]] <- tmp.subs[i, 2]
+      }
+    }
+    return(part)
+  }
+  text.list <- lapply(text.list, replace)
+  text <- sapply(text.list, paste, collapse = " ")
+  return(text)
+}
---
+++ R/spellCorrect.R
@@ -0,0 +1,49 @@
+spellCorrect <- function(text, sensitivity = 0.11) {
+  # auto-spell check 
+  new.length <- 1
+  old.length <- 0
+  subs <- matrix(nrow = 0, ncol = 2)
+  print("Calculating distances")
+  all.words <- table(unlist(strsplit(text, split = " ")))
+  all.words <- sort(all.words)
+  main.dist.matrix <- adist(names(all.words))
+  colnames(main.dist.matrix) <- names(all.words)
+  rownames(main.dist.matrix) <- names(all.words)
+  main.dist.matrix[main.dist.matrix==0] <- NA
+  
+  while(old.length != new.length) {
+    old.length <- new.length 
+    all.words <- sort(table(unlist(strsplit(text, split = " "))))
+    all.words <- all.words[nchar(names(all.words))!=1]
+    
+    dist.matrix <- main.dist.matrix[rownames(main.dist.matrix) %in% 
+                                      names(all.words),
+                                    colnames(main.dist.matrix) %in% names(all.words)]
+    
+    dist.matrix <- dist.matrix[match(names(all.words),
+                                     rownames(dist.matrix)),
+                               match(names(all.words), 
+                                     colnames(dist.matrix))]
+    
+    min.dists <- apply(dist.matrix, MARGIN = 1, min, na.rm = TRUE)
+    min.dist.locs <- apply(dist.matrix, MARGIN = 1, which.min)
+    min.dist.locs[((1:length(min.dist.locs)) - min.dist.locs) > 0] <- NA
+    
+    for(i in 1:ncol(dist.matrix)) {
+      if(!is.na(min.dist.locs[i]) ) {
+        if(min.dists[i] / nchar(names(all.words[i])) < sensitivity) {
+          new.row <- c(names(all.words[i]), 
+                       names(all.words[min.dist.locs[i]]))
+          print(new.row)
+          subs <- rbind(subs, new.row)
+        }
+      }
+    }
+    
+    text <- replaceValues(text, subs)
+    new.length <- nrow(subs)
+  }
+  return(text)
+}
+
+
