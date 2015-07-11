418a05411c2d6407838dc8a107da6bd721dffd96
unknown <jonathan.mellon@nuffield.ox.ac.uk>
2015-07-11 16:05:48 -0400

    updating with option to account for frequencies in original graph

--- R/cooccurrenceToAdjacency.R
+++ R/cooccurrenceToAdjacency.R
@@ -1,17 +1,25 @@
-cooccurenceToAdjacency <- function(term.document.matrix) {
-	# this function counts the cooccurences of two terms in the term.doc matrix
-	countCooccurence <- function(term1, term2, term.doc.matrix) {
-		return(sum(term.doc.matrix[, term1] & term.doc.matrix[, term2], na.rm = T))
-	}
-	#this function fills in the complete adjacency matrix row for one term
-	fillAdjacencyRow <- function(current.term, all.terms) {
-		return(apply(X = as.matrix(all.terms), MARGIN = 1, FUN = countCooccurence, 
-								 term1 = current.term, term.doc.matrix = term.document.matrix))
-	}
-	all.terms <- colnames(term.document.matrix)
-	adjacency <- apply(as.matrix(all.terms), MARGIN = 1, FUN = fillAdjacencyRow,
-										 all.terms = all.terms)
-	colnames(adjacency) <- all.terms
-	rownames(adjacency) <- all.terms
-	return(adjacency)
+cooccurenceToAdjacency <- function (term.document.matrix, sum.weights = FALSE) {
+  if(sum.weights) {
+    countCooccurence <- function(term1, term2, term.doc.matrix) {
+      present <- term.doc.matrix[, term1]!=0 & term.doc.matrix[, term2]!=0
+      sums <- term.doc.matrix[, term1] + term.doc.matrix[, term2]
+      return(sum(sums[present]))
+    }
+    
+  } else {
+    countCooccurence <- function(term1, term2, term.doc.matrix) {
+      return(sum(term.doc.matrix[, term1] & term.doc.matrix[, 
+                                                            term2], na.rm = T))
+    }
+  }
+  fillAdjacencyRow <- function(current.term, all.terms) {
+    return(apply(X = as.matrix(all.terms), MARGIN = 1, FUN = countCooccurence, 
+                 term1 = current.term, term.doc.matrix = term.document.matrix))
+  }
+  all.terms <- colnames(term.document.matrix)
+  adjacency <- apply(as.matrix(all.terms), MARGIN = 1, FUN = fillAdjacencyRow, 
+                     all.terms = all.terms)
+  colnames(adjacency) <- all.terms
+  rownames(adjacency) <- all.terms
+  return(adjacency)
 }
