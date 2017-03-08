d8deefeef8039f070fc33e749a4ba045cc185fd7
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2017-03-08 13:37:08 -0500

    theme_bes

---
+++ R/theme_bes.R
@@ -0,0 +1,13 @@
+theme_bes <- function (base_size = 11, base_family = "") {
+  theme_grey(base_size = base_size, base_family = base_family) %+replace% 
+    theme(panel.background = element_rect(fill = "white", 
+                                          colour = NA), 
+          axis.line = element_line(colour = "grey20"),
+          panel.grid.minor = element_blank(), 
+          panel.grid.major = element_blank(),
+          legend.key = element_rect(fill = "white", 
+                                    colour = NA),
+          axis.text=element_text(size=11),
+          axis.title=element_text(size=11), 
+          complete = FALSE)
+}
