cd69b8cf57b959ab6624204fdb4731e4f54cc1d7
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2017-04-29 19:04:40 -0400

    fiddling about

--- R/assignToBoundaries.R
+++ R/assignToBoundaries.R
@@ -1,15 +1,19 @@
-assignToBoundaries <- function(trans.mat, thing.to.assign) {
+assignToBoundaries <- function(trans.mat, thing.to.assign, labels) {
   # this function expects to receive a transition matrix with weights 
   # that determine what proportion of each row gets assigned to each column 
-  if(all(names(thing.to.assign) %in% rownames(trans.mat))) {
-    warning(paste0(sum(!names(thing.to.assign) %in% rownames(trans.mat)), 
+  if(!all(labels %in% rownames(trans.mat))) {
+    warning(paste0(sum(!labels %in% rownames(trans.mat)), 
                    " origins do not appear in the transition matrix"))
   }
   trans.mat[is.na(trans.mat)] <- 0
   trans.mat <- prop.table(trans.mat, 1)
+  
+  trans.mat <- trans.mat[match(labels, rownames(trans.mat)), ]
+  totals <- colSums(trans.mat * thing.to.assign, na.rm = T)
+  
+  
   if(all(round(rowSums(trans.mat * thing.to.assign), 3)!=round(thing.to.assign, 3))) {
     stop("Total do not reconcile")
   }
-  totals <- colSums(trans.mat * thing.to.assign)
   return(totals)
 }
