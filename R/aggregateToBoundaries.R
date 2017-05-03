9a04b50db61affb93c7cc9a4e16dfdcb74193cf4
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2017-05-03 12:23:57 -0400

    aggregate boundaries

---
+++ R/aggregateToBoundaries.R
@@ -0,0 +1,19 @@
+aggregateToBoundaries <- function(df, identifier.x, weight.x, lookup) {
+  require(mellonMisc)
+  lookup <- lookup[which(lookup[, 1] %in% unlist(df[, identifier.x])), ]
+  colnames(lookup) <- c("identifier.x", "identifier.y")
+  lookup <- dtf(lookup)
+  df <- safemerge(df, lookup, by.x = identifier.x, by.y = "identifier.x", type= "1:m")
+  
+  df.multiple <- split( df[, !colnames(df)%in% c(identifier.x, "identifier.y")],
+                        f = list(df$identifier.y))
+  output.data <- t(sapply(X = df.multiple, 
+                          function(x) apply(as.matrix(x[, 1:ncol(x)]), 2, weighted.mean, 
+                                            na.rm = T,
+                                            w = unlist(x[, weight.x]))))
+  output.data <- dtf(output.data)
+  
+  output.data$identifier.y <- rownames(output.data)
+  output.data[, weight.x] <- NULL
+  return(output.data)
+}
