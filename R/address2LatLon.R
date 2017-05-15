550b2c3733d8e323d75c4574bbd9ebd6dfcc6271
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2017-05-15 13:35:32 -0400

    address2LatLon

---
+++ R/address2LatLon.R
@@ -0,0 +1,18 @@
+address2LatLon <- function(addresses) {
+  require(httr)
+  addresses <- paste(addresses, collapse = "\",\"")
+  addresses <- paste0("[\"", addresses, "\"]")
+  output <- POST('http://www.datasciencetoolkit.org/street2coordinates',
+                 body = addresses, encode = 'json')
+  output <- content(output, "parsed")
+  getLatLon <- function(x) {
+    lat <- x["latitude"]
+    lon <- x["longitude"]
+    lon[is.null(lon)] <- NA
+    lat[is.null(lat)] <- NA
+    c(lat,lon)
+  }
+  output <- t(sapply(output, getLatLon))
+  colnames(output) <- c("lat", "lon")
+  return(output)
+}
