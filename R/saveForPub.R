7e427c0ddbd830c47bbaf17aea1794167cb66806
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2017-07-13 12:09:53 -0400

    save

---
+++ R/saveForPub.R
@@ -0,0 +1,7 @@
+saveForPub <- function(object, file.path, width = 6, height = 6, dpi = 600) {
+  ggsave(object, file = paste0(file.path, ".eps"), width = width, height = height, dpi = dpi)
+  ggsave(object, file = paste0(file.path, ".png"), width = width, height = height, dpi = dpi)
+  ggsave(object, file = paste0(file.path, ".tiff"), 
+         width = width, height = height, compression = "lzw", dpi = dpi)
+  invisible(NULL)
+}
