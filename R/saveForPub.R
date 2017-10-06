3f1011dfbace7f0f8d4ff42fba0cc07993973e9a
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2017-10-06 15:39:43 -0400

    added text resizing option

--- R/saveForPub.R
+++ R/saveForPub.R
@@ -1,4 +1,9 @@
-saveForPub <- function(object, file.path, width = 6, height = 6, dpi = 600) {
+saveForPub <- function(object, file.path, width = 6, height = 6, dpi = 600, text_size  = NULL) {
+  if(!is.null(text_size)) {
+    object <- object + theme(text = element_text(size = text_size), 
+                   axis.text = element_text(size = text_size),
+                   axis.title = element_text(size = text_size))
+  }
   ggsave(object, file = paste0(file.path, ".eps"), width = width, height = height, dpi = dpi)
   ggsave(object, file = paste0(file.path, ".png"), width = width, height = height, dpi = dpi)
   ggsave(object, file = paste0(file.path, ".tiff"), 
