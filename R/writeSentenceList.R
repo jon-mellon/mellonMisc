17c2d071b0f444848886b96ac1c6e13a47094f6a
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2017-11-30 20:25:18 -0500

    function for writing a vector into a sentence list

---
+++ R/writeSentenceList.R
@@ -0,0 +1,16 @@
+writeSentenceList <- function(x, oxcomma = T) {
+  if(length(x) >2) {
+    last.item <- x[length(x)]
+    x <- x[-length(x)]
+    if(oxcomma) {
+      x <- paste0(paste(x, collapse = ", "), ", and ", last.item)  
+    } else {
+      x <- paste0(paste(x, collapse = ", "), " and ", last.item)  
+    }
+    return(x)
+  } 
+  if(length(x)==2) {
+    x <- paste(x, collapse = " and ")
+  }
+  return(x)
+}
