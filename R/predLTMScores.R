4a3fd9931d8800c180e87e25924f0427bb6620c8
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2018-03-06 17:48:23 -0500

    predict grm scores

---
+++ R/predLTMScores.R
@@ -0,0 +1,13 @@
+predGRMScores <- function(data, mod) {
+  vals <- ltm::factor.scores(mod)
+  colnames(vals)
+  vals <- vals$score.dat
+  vars <- vals[, !colnames(vals) %in% c("Obs", "Exp", "z1", "se.z1")]
+  data <- data[, colnames(vars)]
+  # data <- data[rowSums(is.na(data))<ncol(vars), ]
+  index <- apply(data, 1, paste0, collapse = "|")
+  index.vars <- apply(vars, 1, paste0, collapse = "|")
+  output <- dtf(z = vals$z1[match(index, index.vars)], 
+                se = vals$se.z1[match(index, index.vars)])
+  return(output)
+}
