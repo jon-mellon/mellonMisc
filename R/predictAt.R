ca1fd0fac51588a3bea3c17a3e01cfbf86d213cc
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2018-03-06 15:48:20 -0500

    updating to support weights for predictat

--- R/predictAt.R
+++ R/predictAt.R
@@ -9,13 +9,13 @@ predictAt <- function(mod, at ) {
     if(any(!vars %in% names(data))) {
       stop("Variable not found in data")
     }
-    
     for(jj in vars) {
       data[, jj] <- at[ii, jj]
     }
     pred.prob <- predict(mod, newdata = data, type = "response", se.fit = T)
-    at[ii, "prob"] <- mean(pred.prob$fit)
-    at[ii, "se"]   <- sum(pred.prob$se.fit) / (length(pred.prob$se.fit) -1 )
+    
+    at[ii, "prob"] <- wtd.mean(pred.prob$fit, weights = data$`(weights)`, na.rm = T)
+    at[ii, "se"]   <- wtd.mean(pred.prob$se.fit, weights = data$`(weights)`, na.rm = T)
   }
   at$lci <- at$prob - 1.96 * at$se
   at$uci <- at$prob + 1.96 * at$se
