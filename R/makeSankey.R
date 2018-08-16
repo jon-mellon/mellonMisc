160a7d485d74bffc7809e5f0ece5621fdfe82bcf
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2018-08-16 11:14:10 -0400

    small fixes

--- R/makeSankey.R
+++ R/makeSankey.R
@@ -1,34 +1,34 @@
 
 
 makeSankey <- function(switch, origin.date, dest.date, file = "sankey.html") {
-  colnames(switch) <- c("Origin", "Destination", "Percent")
-  
-  switch$Origin <- paste(switch$Origin, origin.date)
-  switch$Destination <- paste(switch$Destination, dest.date)
-  
-  party.list <- unique(as.vector(mapply(c, switch$Origin, switch$Destination)))
-  party.list <- gsub(paste0(" +", origin.date), "", party.list)
-  party.list <- gsub(paste0(" +", dest.date), "", party.list)
-  
-  party.colours <- c(Conservative = "#0087dc", Labour = "#d50000", `Lib Dem` = "#FDBB30", 
-                     UKIP = "#B3009D", Green = "#008066", SNP = "#FFF95D", Plaid = "#3F8428",
-                     DK = "#a4a6a8", Other = "#a4a6a8")
-  party.list <- paste0("'", party.colours[party.list], "'")
-  
-  party.list <- paste(party.list, collapse = ", ")
-  
-  makeLink <- function(label1, label2, count) {
-    paste0("[ '", label1, "' , '", label2, "' , ", count, "],")
-  }
-  switch$row <- NA
-  for(ii in 1:nrow(switch)) {
-    switch$row[ii] <- makeLink(switch$Origin[ii], switch$Destination[ii], 
-                               switch$Percent[ii])
-  }
-  links <- paste(switch$row, collapse = "\n")
-  
-  
-  head <- "<html>
+	colnames(switch) <- c("Origin", "Destination", "Percent")
+	
+	switch$Origin <- paste(switch$Origin, origin.date)
+	switch$Destination <- paste(switch$Destination, dest.date)
+	
+	party.list <- unique(as.vector(mapply(c, switch$Origin, switch$Destination)))
+	party.list <- gsub(paste0(" +", origin.date), "", party.list)
+	party.list <- gsub(paste0(" +", dest.date), "", party.list)
+	
+	party.colours <- c(Conservative = "#0087dc", Labour = "#d50000", `Lib Dem` = "#FDBB30", 
+										 UKIP = "#B3009D", Green = "#008066", SNP = "#FFF95D", Plaid = "#3F8428",
+										 DK = "#a4a6a8", Other = "#a4a6a8", BNP = "#000000")
+	party.list <- paste0("'", party.colours[party.list], "'")
+	
+	party.list <- paste(party.list, collapse = ", ")
+	
+	makeLink <- function(label1, label2, count) {
+		paste0("[ '", label1, "' , '", label2, "' , ", count, "],")
+	}
+	switch$row <- NA
+	for(ii in 1:nrow(switch)) {
+		switch$row[ii] <- makeLink(switch$Origin[ii], switch$Destination[ii], 
+															 switch$Percent[ii])
+	}
+	links <- paste(switch$row, collapse = "\n")
+	
+	
+	head <- "<html>
   <head>
   <script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>
   <script type=\"text/javascript\">
@@ -41,15 +41,15 @@ makeSankey <- function(switch, origin.date, dest.date, file = "sankey.html") {
   data.addColumn('string', '2015 Vote');
   data.addColumn('number', 'Vote');
   data.addRows(["
-  
-  
-  
-  mid <- "]);
+	
+	
+	
+	mid <- "]);
   
   // Sets chart options.
   var colors = [ "
-  
-  end <- "];
+	
+	end <- "];
   
   
   var options = {
@@ -76,8 +76,8 @@ makeSankey <- function(switch, origin.date, dest.date, file = "sankey.html") {
   <div id=\"sankey_basic\" style=\"width: 900px; height: 300px;\"></div>
   </body>
   </html>"
-  
-  writeLines(paste(head, 
-                   links, 
-                   mid, party.list, end, collapse = "\n"), con = file)
+	
+	writeLines(paste(head, 
+									 links, 
+									 mid, party.list, end, collapse = "\n"), con = file)
 }
--- R/saveForPub.R
+++ R/saveForPub.R
@@ -4,10 +4,11 @@ saveForPub <- function(object, file.path, width = 6, height = 6, dpi = 600, text
                    axis.text = element_text(size = text_size),
                    axis.title = element_text(size = text_size))
   }
-  ggsave(object, file = paste0(file.path, ".eps"), width = width, height = height, dpi = dpi)
+  
   ggsave(object, file = paste0(file.path, ".png"), width = width, height = height, dpi = dpi)
   ggsave(object, file = paste0(file.path, ".tiff"), 
          width = width, height = height, compression = "lzw", dpi = dpi)
   save(object, file = paste0(file.path, ".rda"))
+  ggsave(object, file = paste0(file.path, ".eps"), width = width, height = height, dpi = dpi)
   invisible(NULL)
 }
