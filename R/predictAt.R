be2270479a3adc360e3ba874b4d375f8b020af21
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2019-01-24 21:32:32 -0500

    allowing specific combinations of prediction levels to be passed rather than just all combinations

--- R/predictAt.R
+++ R/predictAt.R
@@ -1,9 +1,12 @@
-predictAt <- function(mod, at ) {
+predictAt <- function(mod, at, multiply.out = T, se = T) {
 	vars <- names(at)
+	if(multiply.out) {
 	at <- expand.grid(at)
+	}
 	at$prob <- NA
-	at$se <- NA
-	
+	if(se) {
+	  at$se <- NA
+	}
   for(ii in 1:nrow(at)) {
     data <- model.frame(mod)
     if(any(!vars %in% names(data))) {
@@ -12,12 +15,20 @@ predictAt <- function(mod, at ) {
     for(jj in vars) {
       data[, jj] <- at[ii, jj]
     }
-    pred.prob <- predict(mod, newdata = data, type = "response", se.fit = T)
+    pred.prob <- predict(mod, newdata = data, type = "response", se.fit = se)
+    if(!se) {
+      pred.prob <- dtf(fit = pred.prob)
+    }
     
-    at[ii, "prob"] <- wtd.mean(pred.prob$fit, weights = data$`(weights)`, na.rm = T)
-    at[ii, "se"]   <- wtd.mean(pred.prob$se.fit, weights = data$`(weights)`, na.rm = T)
+    at[ii, "prob"] <- Hmisc::wtd.mean(pred.prob$fit, weights = data$`(weights)`, na.rm = T)
+    if(se) {
+      at[ii, "se"]   <- wtd.mean(pred.prob$se.fit, weights = data$`(weights)`, na.rm = T)  
+    }
+    
   }
+	if(se) {
   at$lci <- at$prob - 1.96 * at$se
   at$uci <- at$prob + 1.96 * at$se
+	}
   return(at)
 }
