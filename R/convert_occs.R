aa1f4dd82c38d2e0dc8965f987cec6c9b6a764df
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2019-03-13 11:53:33 -0400

    occ_crosswalks

---
+++ R/convert_occs.R
@@ -0,0 +1,19 @@
+convertOccs <- function(occs, from, to ) {
+  occ.edges <- do.call(rbind, strsplit(names(occ.crosswalks), "-"))
+  library(igraph)
+  occ.graph <- graph.edgelist(occ.edges, directed = T)
+  
+  # plot(occ.graph)
+  relevant.crosswalks <- all_shortest_paths(occ.graph, from = from, to = to)$res[[1]]
+  
+  cw.sets <- c()
+  for(ii in 1:(length(relevant.crosswalks) - 1)) {
+    cw.sets[ii] <- paste(names(relevant.crosswalks[ii:(ii+1)]), collapse = "-")
+  }
+  
+  for(jj in cw.sets) {
+    occs <- occ.crosswalks[[jj]][, 2][match(occs, occ.crosswalks[[jj]][, 1])]
+  }
+  return(occs)
+}
+
