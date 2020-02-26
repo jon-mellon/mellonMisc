a943590dd46cee28c9e76705a5375820e51d7aa6
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2020-02-26 14:19:59 -0500

    remove missing predictions

--- R/predLTMScores.R
+++ R/predLTMScores.R
@@ -9,5 +9,6 @@ predGRMScores <- function(data, mod) {
   index.vars <- apply(vars, 1, paste0, collapse = "|")
   output <- dtf(z = vals$z1[match(index, index.vars)], 
                 se = vals$se.z1[match(index, index.vars)])
+  output[rowSums(is.na(data))==ncol(vars), ] <- NA
   return(output)
 }
