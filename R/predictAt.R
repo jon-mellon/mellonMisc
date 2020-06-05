c957a67f07aba4e0dba2fad19b3cac2c36c7c678
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2020-06-05 14:09:29 -0400

    predictat update

--- R/predictAt.R
+++ R/predictAt.R
@@ -20,6 +20,10 @@ predictAt <- function(mod, at, multiply.out = T, se = T) {
     if(!se) {
       pred.prob <- dtf(fit = pred.prob)
     }
+    if(attributes(pred.prob)=="var") {
+    	pred.prob <- dtf(fit = as.vector(pred.prob), se.fit = sqrt(attributes(pred.prob)$var))
+    }
+    
     
     at[ii, "prob"] <- Hmisc::wtd.mean(pred.prob$fit, weights = data$`(weights)`, na.rm = T)
     if(se) {
