b637125bf14bf94aed1070ae9828867cb6c77214
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2020-11-20 16:46:24 -0500

    adding legend selector

--- R/grid_arrange_shared_legend.R
+++ R/grid_arrange_shared_legend.R
@@ -1,11 +1,11 @@
 #' @export grid_arrange_shared_legend
 grid_arrange_shared_legend <- function (..., nrow = 1, ncol = length(plots), 
-																						 position = c("bottom", "right")) 
+																						 position = c("bottom", "right"), legend.index = 1) 
 {
 	# browser()
 	plots <- list(...)
 	position <- match.arg(position)
-	g <- ggplotGrob(plots[[1]] + theme(legend.position = position))$grobs
+	g <- ggplotGrob(plots[[legend.index]] + theme(legend.position = position))$grobs
 	legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
 	legend$grobs[[1]]$grobs[[1]]$width[[1]] <- ncol + 1
 	lheight <- sum(legend$height)
