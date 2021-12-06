dc8bb460b538569f2165e16e75f696e22847e922
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2021-12-06 16:15:54 -0500

    better message suppression

--- R/saveForPub.R
+++ R/saveForPub.R
@@ -1,16 +1,22 @@
 #' @export saveForPub
-saveForPub <- function(object, file.path, width = 6, height = 6, dpi = 600, text_size  = NULL) {
+saveForPub <- function(object, file.path, width = 6, height = 6, 
+											 dpi = 600, text_size  = NULL, silent = TRUE) {
   if(!is.null(text_size)) {
     object <- object + theme(text = element_text(size = text_size), 
                    axis.text = element_text(size = text_size),
                    axis.title = element_text(size = text_size))
   }
   
-  try(ggsave(object, file = paste0(file.path, ".png"), width = width, height = height, dpi = dpi))
+  try(ggsave(object, file = paste0(file.path, ".png"), 
+  					 width = width, height = height, dpi = dpi), silent = silent)
   try(ggsave(object, file = paste0(file.path, ".tiff"), 
-         width = width, height = height, compression = "lzw", dpi = dpi))
-  try(save(object, file = paste0(file.path, ".rda")))
-  try(ggsave(object, file = paste0(file.path, ".eps"), width = width, height = height, dpi = dpi))
-  try(ggsave(object, file = paste0(file.path, ".svg"), width = width, height = height, dpi = dpi))
+         width = width, height = height, compression = "lzw", dpi = dpi), 
+  		silent = silent)
+  try(save(object, file = paste0(file.path, ".rda")), 
+  		silent = silent)
+  try(ggsave(object, file = paste0(file.path, ".eps"), 
+  					 width = width, height = height, dpi = dpi), silent = silent)
+  try(ggsave(object, file = paste0(file.path, ".svg"), 
+  					 width = width, height = height, dpi = dpi), silent = silent)
   invisible(NULL)
 }
