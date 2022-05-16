cc1d91d2aaadce3ac1e39ef1c169927e32a11904
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2022-05-16 14:27:54 -0400

    fixing legends

--- R/grid_arrange_shared_legend.R
+++ R/grid_arrange_shared_legend.R
@@ -1,14 +1,13 @@
 #' @export grid_arrange_shared_legend
 grid_arrange_shared_legend <- function (..., nrow = 1, ncol = length(plots), 
-																						 position = c("bottom", "right"), legend.index = 1, 
-																				left = NULL, bottom = NULL, right = NULL, top = NULL) 
-{
+																				position = c("bottom", "right"), legend.index = 1, 
+																				left = NULL, bottom = NULL, right = NULL, top = NULL) {
 	# browser()
 	plots <- list(...)
 	position <- match.arg(position)
 	g <- ggplotGrob(plots[[legend.index]] + theme(legend.position = position))$grobs
 	legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
-	legend$grobs[[1]]$grobs[[1]]$width[[1]] <- grid::unit(ncol + 1, "npc")
+	legend$grobs[[1]]$grobs[[1]]$width[[1]] <- grid::unit(ncol + 5, "npc")
 	lheight <- sum(legend$height)
 	lwidth <- sum(legend$width)
 	gl <- lapply(plots, function(x) x + theme(legend.position = "none"))
